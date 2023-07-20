89a47326027555f1482fdd1c3cc783a4b6dce109
KingKyoTrue <tanadicoding@gmail.com>
2023-07-20 16:30:36 +0700

    update

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -4,6 +4,7 @@ import Spinner from "../components/Spinner"
 import ClientInfo from "../components/ClientInfo"
 import { GET_PROJECT } from "../queries/projectQueries"
 import DeleteProjectButton from "../components/DeleteProjectButton"
+import EditProjectForm from "../components/EditProjectForm"
 
 export default function Project() {
     const { id } = useParams();
@@ -29,6 +30,8 @@ export default function Project() {
             <p className="lead">{ data.project.status }</p>
 
             <ClientInfo client={data.project.client} />
+
+            <EditProjectForm project={data.project} />
 
             <DeleteProjectButton projectId={data.project.id} />
         </div>
