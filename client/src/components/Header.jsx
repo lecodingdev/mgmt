7c4b19b0a8389f6c9e31950eb9dae33b30134265
KingKyoTrue <tanadicoding@gmail.com>
2023-07-21 12:32:53 +0700

    update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
             <a className='navbar-brand' href="/">
                 <div className="d-flex">
                     <img src={logo} alt="logo" className='mr-2' />
-                    <div>ProjectMgmt</div>
+                    <div>LeCodingDev Project Management</div>
                 </div>
             </a>
         </div>
